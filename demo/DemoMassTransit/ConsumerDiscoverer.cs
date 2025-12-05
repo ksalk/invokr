b603ec1f138a06b0ca13e42fdeaccc4355409cf6
Konrad Sałkowski <konrad.salkowski@gmail.com>
2025-12-05 14:40:10 +0100

    MT consumers discovery

---
+++ demo/DemoMassTransit/ConsumerDiscoverer.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using MassTransit;
+using MassTransit.Metadata;
+
+public class ConsumerDiscoverer
+{
+    public static void DiscoverConsumers(IServiceProvider serviceProvider)
+    {
+        var assemblies = new[] { Assembly.GetEntryAssembly() };
+
+        var consumers = assemblies
+            .SelectMany(a => a.GetTypes())
+            .Where(t => RegistrationMetadata.IsConsumer(t) && !t.IsInterface && !t.IsAbstract)
+            .ToList();
+        Console.WriteLine($"Discovered {consumers.Count} consumers via IServiceProvider.");
+    }
+}
--- demo/DemoMassTransit/Program.cs
+++ demo/DemoMassTransit/Program.cs
@@ -29,6 +29,8 @@ builder.Services.AddScoped<ConsumerInvoker>();
 
 var app = builder.Build();
 
+ConsumerDiscoverer.DiscoverConsumers(app.Services);
+
 app.MapGet("/invoke", async (ConsumerInvoker invoker) =>
 {
     await invoker.Invoke();
